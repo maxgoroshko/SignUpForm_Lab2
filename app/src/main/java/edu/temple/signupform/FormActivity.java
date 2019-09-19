ff618ce4e2524987e48fbaad2987aa54da6769ac
MaxGoroshkoTU <tug47632@temple.edu>
2019-09-19 16:50:58 -0400

    Values initialized

--- app/src/main/java/edu/temple/signupform/FormActivity.java
+++ app/src/main/java/edu/temple/signupform/FormActivity.java
@@ -3,6 +3,7 @@ package edu.temple.signupform;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -14,9 +15,21 @@ public class FormActivity extends AppCompatActivity {
     EditText email;
     Button signUp;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.nameText);
+        password = findViewById(R.id.passwordText);
+        rePassword = findViewById(R.id.rePasswordText);
+        email = findViewById(R.id.emailText);
+        signUp = findViewById(R.id.signUp);
+
     }
+
+
+
+
 }
