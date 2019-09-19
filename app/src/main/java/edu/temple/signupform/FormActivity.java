8d8143c84483df146230d988927fefa39dedd09d
MaxGoroshkoTU <tug47632@temple.edu>
2019-09-19 15:02:53 -0400

    Local fields created; Button widget added;

--- app/src/main/java/edu/temple/signupform/FormActivity.java
+++ app/src/main/java/edu/temple/signupform/FormActivity.java
@@ -3,8 +3,16 @@ package edu.temple.signupform;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class FormActivity extends AppCompatActivity {
+
+    EditText name;
+    EditText password;
+    EditText rePassword;
+    EditText email;
+    Button signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
