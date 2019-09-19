2a4c946bcce33ebc74ce641bdf2c7f5d9256143d
MaxGoroshkoTU <tug47632@temple.edu>
2019-09-19 17:14:10 -0400

    Name check completed

--- app/src/main/java/edu/temple/signupform/FormActivity.java
+++ app/src/main/java/edu/temple/signupform/FormActivity.java
@@ -3,9 +3,11 @@ package edu.temple.signupform;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class FormActivity extends AppCompatActivity {
 
@@ -27,9 +29,30 @@ public class FormActivity extends AppCompatActivity {
         email = findViewById(R.id.emailText);
         signUp = findViewById(R.id.signUp);
 
+
+        signUp.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                checkDataEntered();
+
+            }
+        });
     }
 
+    boolean isEmpty(EditText text)
+    {
+        CharSequence checkText = text.getText().toString();
+        return TextUtils.isEmpty(checkText);
+    }
 
+    void checkDataEntered()
+    {
+        if(isEmpty(name))
+        {
+            Toast.makeText(this, "You got to input name", Toast.LENGTH_SHORT).show();
+        }
+    }
 
 
 }
